c06fdc446cb2b24ca1bab27d8c10c53aec3fb754
Reherrmann <94018742+Reherrmann@users.noreply.github.com>
2026-07-29 15:38:45 -0300

    Update config.js

--- bergamo/js/config.js
+++ bergamo/js/config.js
@@ -1,10 +1,10 @@
 // js/config.js
 const PLURI_CONFIG = {
     appsScript: {
-        baseUrl: 'https://script.google.com/macros/s/AKfycbxaQHRuscMmF24PgR6ftLleOKGqoxw-iHaBcunJZv0UODFdiYM5FghV9wjSAo-5fKqKdA/exec',
-        pacientes: 'https://script.google.com/macros/s/AKfycbxaQHRuscMmF24PgR6ftLleOKGqoxw-iHaBcunJZv0UODFdiYM5FghV9wjSAo-5fKqKdA/exec?action=pacientes',
-        agendamentos: 'https://script.google.com/macros/s/AKfycbxaQHRuscMmF24PgR6ftLleOKGqoxw-iHaBcunJZv0UODFdiYM5FghV9wjSAo-5fKqKdA/exec?action=agendamentos',
-        equipe: 'https://script.google.com/macros/s/AKfycbxaQHRuscMmF24PgR6ftLleOKGqoxw-iHaBcunJZv0UODFdiYM5FghV9wjSAo-5fKqKdA/exec?action=equipe',
-        salvarAgendamento: 'https://script.google.com/macros/s/AKfycbxaQHRuscMmF24PgR6ftLleOKGqoxw-iHaBcunJZv0UODFdiYM5FghV9wjSAo-5fKqKdA/exec',
+        baseUrl: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec',
+        pacientes: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec?action=pacientes',
+        agendamentos: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec?action=agendamentos',
+        equipe: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec?action=equipe',
+        salvarAgendamento: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec',
     },
 };
