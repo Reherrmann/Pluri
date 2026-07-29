32da57ea7f85b0a23ed9c0987ae61fa2325c0765
Reherrmann <94018742+Reherrmann@users.noreply.github.com>
2026-07-29 18:56:16 -0300

    Update config.js

--- bergamo/js/config.js
+++ bergamo/js/config.js
@@ -1,10 +1,12 @@
 // js/config.js
 const PLURI_CONFIG = {
     appsScript: {
-        baseUrl: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec',
-        pacientes: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec?action=read&sheet=Pacientes',
-        agendamentos: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec?action=read&sheet=Agendamentos',
-        equipe: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec?action=read&sheet=Equipe',
-        salvarAgendamento: 'https://script.google.com/macros/s/AKfycbx9jZBESxL9OWkB7qqbRMipoNw9DtQTzmyfPhLUe-dORFDpuxgXpZHtF8izrNUFsmaI/exec',
+        baseUrl: 'https://script.google.com/macros/s/AKfycbzJlICIugAAIO_7S7YYcarQqscb6CN4sJ0riYCDbf7p_4ns04bCrV19KhnACHgqoqTI/exec',
+        pacientes: 'https://script.google.com/macros/s/AKfycbzJlICIugAAIO_7S7YYcarQqscb6CN4sJ0riYCDbf7p_4ns04bCrV19KhnACHgqoqTI/exec?action=read&sheet=Pacientes',
+        agendamentos: 'https://script.google.com/macros/s/AKfycbzJlICIugAAIO_7S7YYcarQqscb6CN4sJ0riYCDbf7p_4ns04bCrV19KhnACHgqoqTI/exec?action=read&sheet=Agendamentos',
+        equipe: 'https://script.google.com/macros/s/AKfycbzJlICIugAAIO_7S7YYcarQqscb6CN4sJ0riYCDbf7p_4ns04bCrV19KhnACHgqoqTI/exec?action=read&sheet=Equipe',
+        salvarAgendamento: 'https://script.google.com/macros/s/AKfycbzJlICIugAAIO_7S7YYcarQqscb6CN4sJ0riYCDbf7p_4ns04bCrV19KhnACHgqoqTI/exec',
+        calendarEvents: 'https://script.google.com/macros/s/AKfycbzJlICIugAAIO_7S7YYcarQqscb6CN4sJ0riYCDbf7p_4ns04bCrV19KhnACHgqoqTI/exec?action=calendar',
+        calendarEventsDate: 'https://script.google.com/macros/s/AKfycbzJlICIugAAIO_7S7YYcarQqscb6CN4sJ0riYCDbf7p_4ns04bCrV19KhnACHgqoqTI/exec?action=calendar&date=',
     },
 };
