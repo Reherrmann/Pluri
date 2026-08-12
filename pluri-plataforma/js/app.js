2a1766ff522e53e78e1e6b69c5424f067fdef468
Reherrmann <94018742+Reherrmann@users.noreply.github.com>
2026-08-12 11:35:54 -0300

    Fix formatting issues in app.js

--- pluri-plataforma/js/app.js
+++ pluri-plataforma/js/app.js
@@ -475,7 +475,7 @@
     <div class="card-placeholder">Espaço adaptável para a clínica</div>
     <div class="card-placeholder">Espaço adaptável para a clínica</div>
 </div>`;
-
+    }
     // ======================================================
     // AGENDA – idêntica ao agenda.js original
     // ======================================================
